d8824d69dc096a3fdf855eb5bdf4e2662a379ff1
Jermaine Dafoe <etesfaye@hotmail.com>
2023-12-29 23:34:51 +0300

    get server running

---
+++ index.js
@@ -0,0 +1,24 @@
+require('dotenv').config()
+const Fastify = require('fastify');
+const fastifyMongooseAPI = require('fastify-mongoose-api');
+const fastifyCors = require('@fastify/cors');
+const fastifyFormbody = require('@fastify/formbody');
+const fastifySwagger = require('@fastify/swagger');
+
+async function bootstrap() {
+    try {
+        const fastify = Fastify({ logger: true });
+        fastify.register(fastifyCors);
+        fastify.register(fastifyFormbody);
+
+
+        await fastify.ready();
+        const PORT = process.env.PORT || 4000;
+        const address = await fastify.listen({port: PORT, host: '0.0.0.0'});
+        console.log(`Server listening on address: ${address}`);
+    } catch (err) {
+        console.error(`Can't start server: ${err.message}`);
+    }
+}
+
+bootstrap();
